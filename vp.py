b049eedbace4b51679c9f64e8933398f09d63884
Bardur Thomsen <brur88@gmail.com>
2025-01-19 15:44:32 -0500

    add data provider for crypto

--- vp.py
+++ vp.py
@@ -4,14 +4,19 @@ import time
 from collections import defaultdict, OrderedDict
 import matplotlib.pyplot as plt
 from datetime import datetime
+import pandas as pd
+from urllib.parse import quote
+from io import StringIO
 
-OPEN_WEIGHT = 0.3
+OPEN_WEIGHT = 0.25
 HIGH_WEIGHT = 0.2
+MID_WEIGHT = 0.1
 LOW_WEIGHT = 0.2
-CLOSE_WEIGHT = 0.3
+CLOSE_WEIGHT = 0.25
 
-def fetch_data(symbol, api_key):
-    url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}&outputsize=full&datatype=csv"
+def fetch_data_av(symbol, api_key):
+    #url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}&outputsize=full&datatype=csv"
+    url = "https://raw.githubusercontent.com/bardurt/volume-by-price/refs/heads/main/daily_riot%20(1).csv"
     try:
         response = requests.get(url)
         response.raise_for_status()
@@ -19,23 +24,55 @@ def fetch_data(symbol, api_key):
     except requests.exceptions.RequestException as e:
         print(f"Error fetching data: {e}")
         return []
+    
+def fetch_data_crypto(symbol):
+    url = f"https://www.cryptodatadownload.com/cdd/Bitstamp_{quote(symbol.upper(), safe='')}USD_d.csv"
+    
+    try:
+        print(f"Fetching data from: {url}")
+        
+        response = requests.get(url)
+        response.raise_for_status()
+        
+        csv_data = StringIO(response.text)
+        
+        df = pd.read_csv(csv_data, skiprows=1)
+        
+     
+        df['date'] = pd.to_datetime(df['date']).dt.date.astype(str)
+        
+        # Normalize the data set
+        result = df[['date', 'open', 'high', 'low', 'close', 'Volume USD']]
+        
+        # Convert DataFrame to list of lists
+        array_format = result.values.tolist()
+        
+        return array_format
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
+        return []
+    except pd.errors.ParserError as e:
+        print(f"Error parsing CSV: {e}")
+        return []
+    
 
 def calculate_volume_by_price(rows):
     price_dict = defaultdict(int)
-    total_volume = 0
 
     for row in rows:
         try:
             volume = int(row[5])
-            total_volume += volume
 
             open_price = float(row[1])
             high_price = float(row[2])
             low_price = float(row[3])
             close_price = float(row[4])
+            mid_price = (high_price - low_price) / 2
 
             price_dict[open_price] += int(volume * OPEN_WEIGHT)
             price_dict[high_price] += int(volume * HIGH_WEIGHT)
+            price_dict[mid_price] += int(volume * MID_WEIGHT)
             price_dict[low_price] += int(volume * LOW_WEIGHT)
             price_dict[close_price] += int(volume * CLOSE_WEIGHT)
 
@@ -83,12 +120,17 @@ def calculate_monthly_changes(rows):
 def calculate_daily_percentage_changes(rows):
     yearly_data = defaultdict(list)
 
+    year_end = datetime.strptime(rows[0][0], "%Y-%m-%d").year
+    year_start = datetime.strptime(rows[len(rows)-1][0], "%Y-%m-%d").year
+
     for row in rows:
         try:
             date = datetime.strptime(row[0], "%Y-%m-%d")
             close_price = float(row[4])
             year_key = date.year
-            yearly_data[year_key].append((date, close_price))
+              # Only append if it's not the first or last item
+            if year_key != year_end and year_key != year_start:
+                yearly_data[year_key].append((date, close_price))
 
         except ValueError:
             continue
@@ -117,7 +159,7 @@ def plot_results(price_dict, monthly_changes, daily_changes, symbol):
     sorted_prices, sorted_volumes = zip(*sorted(zip(prices, weighted_volumes)))
 
     ax1.barh(sorted_prices, sorted_volumes, color='blue')
-    ax1.set_ylabel('Price Levels')
+    ax1.set_ylabel('Price')
     ax1.set_title('Volume by Price')
     ax1.grid(True)
 
@@ -127,8 +169,8 @@ def plot_results(price_dict, monthly_changes, daily_changes, symbol):
     changes = [monthly_changes.get(f"{i:02}", 0) for i in range(1, 13)]
 
     ax2.bar(months, changes, color='orange')
-    ax2.set_ylabel('Average Monthly Change')
-    ax2.set_title('Average Monthly Change in Price Over Years')
+    ax2.set_ylabel('%')
+    ax2.set_title('Average Monthly Change')
     ax2.grid(True)
 
     # Plot Daily Percentage Changes (Averaged)
@@ -136,7 +178,7 @@ def plot_results(price_dict, monthly_changes, daily_changes, symbol):
     for interval, changes in daily_changes.items():
         ax3.plot(range(1, len(changes) + 1), changes, label=f'{interval}')
 
-    ax3.set_ylabel('Daily % Change')
+    ax3.set_ylabel('%')
     ax3.set_xlabel('Day of Year')
     ax3.legend(loc='upper left')
     ax3.grid(True)
@@ -148,9 +190,20 @@ def plot_results(price_dict, monthly_changes, daily_changes, symbol):
 def start(symbol, api_key):
     start_time = time.time()
 
-    rows = fetch_data(symbol, api_key)
-    if not rows:
+    if api_key == "":
+        rows = fetch_data_crypto(symbol)
+    else: 
+        rows = fetch_data_av(symbol, api_key)
+
+    if len(rows) < 3:
+        print("Data invalid, please check if Symbol is correct or access valid")
         return
+
+    if not rows:
+        print("Could not fetch data")
+        return
+    
+    print("fetched ", len(rows), " rows")
 
     price_dict = calculate_volume_by_price(rows)
     monthly_changes = calculate_monthly_changes(rows)
@@ -163,9 +216,13 @@ def start(symbol, api_key):
 
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) != 3:
-        print("Error: Requires 2 arguments (symbol and API key)")
+    if len(sys.argv) < 2:
+        print("Error: Requires at least 1 arguments (symbol)")
     else:
-        symbol = sys.argv[1]
-        api_key = sys.argv[2]
-        start(symbol, api_key)
+        if len(sys.argv)== 3:
+            symbol = sys.argv[1]
+            api_key = sys.argv[2]
+            start(symbol, api_key)
+        else:
+            symbol = sys.argv[1]
+            start(symbol, "")
